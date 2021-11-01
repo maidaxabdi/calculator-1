a7654abb0fe68a09e439ae8268f9eab66881a414
Maida Abdi <91497873+maidaxabdi@users.noreply.github.com>
2021-10-31 23:29:14 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -28,7 +28,7 @@ while True:
         num3 = tokens[3]
 
     # A place to store the return value of the math function we call,
-    # to give us one clear place where that result is printed.
+    # to give us one clear place where that result is printed
     result = None
 
     if not num1.isdigit() or not num2.isdigit():
